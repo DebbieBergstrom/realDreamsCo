29d193172900ca70dad665eb9ac42a2910f949c5
Debbie Bergstrom <debbiebergstroem@gmail.com>
2024-03-03 11:00:05 +0000

    #53 feat(admin): customize products dashboard display

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
 from .models import Product, Category
 
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'get_category_friendly_name',
+        'price',
+        'duration',
+        'available',
+        'image',
+        'created_at',
+    )
 
+    prepopulated_fields = {"slug": ("name",)}
+    ordering = ('name',)
+
+    def get_category_friendly_name(self, obj):
+        return obj.category.friendly_name if obj.category else '-'
+    get_category_friendly_name.short_description = 'Category'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
