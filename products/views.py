ab9e147c46052d28b602a71756d70cf19e78476b
Debbie Bergstrom <debbiebergstroem@gmail.com>
2024-03-04 22:50:32 +0000

    #32 feat(search field): add extended queries to the search form

--- products/views.py
+++ products/views.py
@@ -23,7 +23,8 @@ def all_products(request):
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
             
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(category__name__icontains=query) | Q(dream_center__name__icontains=query)
+
             products = products.filter(queries)
 
     context = {
