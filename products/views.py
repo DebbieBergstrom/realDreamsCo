b63f8af050ec8e129cfbcc6e130118c5a7a59fca
Debbie Bergstrom <debbiebergstroem@gmail.com>
2024-03-04 20:23:05 +0000

    #31 feat(search form): add search functionality to header search ar

--- products/views.py
+++ products/views.py
@@ -1,14 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
 def all_products(request):
-    """ A view to show a detailed page of each product."""
+    """ A view to show all products, plus sorting and search query functionality"""
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "No search criteria entered")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
