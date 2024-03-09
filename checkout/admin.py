85d3fe4866393a4835316b501ec0111e0d9e96bf
Debbie Bergstrom <debbiebergstroem@gmail.com>
2024-03-09 12:01:00 +0000

    #37 feat(checkout): add forms, admin and signals

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ("lineitem_total",)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = (
+        "order_number",
+        "date",
+        "consultation_cost",
+        "order_total",
+        "grand_total",
+    )
+
+    fields = (
+        "order_number",
+        "date",
+        "full_name",
+        "email",
+        "phone_number",
+        "country",
+        "consultation_cost",
+        "order_total",
+        "grand_total",
+    )
+
+    list_display = (
+        "order_number",
+        "date",
+        "full_name",
+        "order_total",
+        "consultation_cost",
+        "grand_total",
+    )
+
+    ordering = ("-date",)
+
+
+admin.site.register(Order, OrderAdmin)
