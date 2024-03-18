5bfcd9e13b9ab1ee2fe9099eb1a1496f849653d6
Debbie Bergstrom <debbiebergstroem@gmail.com>
2024-03-18 13:21:50 +0000

    #48 feat(product admin): add form template, view and url

--- products/forms.py
+++ products/forms.py
@@ -1,37 +1,30 @@
 from django import forms
 from .models import Product, Category, DreamCenter
-from .widgets import CustomClearableFileInput
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = "__all__"
+        exclude = ("slug",)
         widgets = {
-            "image": CustomClearableFileInput,
             "dream_center": forms.CheckboxSelectMultiple,
         }
 
-    size = forms.ChoiceField(
-        choices=Product.SIZE_CHOICES,
-        initial="S",
-        label=Product._meta.get_field("size").verbose_name,
-    )
-    image = forms.ImageField(
-        label=Product._meta.get_field("image").verbose_name, required=True
-    )
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        # Categories with friendly names
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
-
         self.fields["category"].choices = friendly_names
-        self.fields["category"].label = Category._meta.verbose_name
-        self.fields["category"].required = True
-        self.fields["dream_center"].queryset = DreamCenter.objects.all()
-        self.fields["dream_center"].required = True
-        self.fields["dream_center"].label = DreamCenter._meta.verbose_name_plural
 
+        # Dream Centers
+        dream_centers = DreamCenter.objects.all()
+        self.fields["dream_center"].queryset = dream_centers
+        self.fields["dream_center"].label = DreamCenter._meta.verbose_name_plural
+        self.fields["dream_center"].required = True
+
+        # Adds a class to all fields
         for field_name, field in self.fields.items():
             field.widget.attrs["class"] = "border-black rounded-2"
