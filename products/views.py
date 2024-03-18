68abbdbee2ec69b5336fbd01135b28a56ff24085
Debbie Bergstrom <debbiebergstroem@gmail.com>
2024-03-18 14:19:48 +0000

    # feat(admin products): add product update functionality

--- products/views.py
+++ products/views.py
@@ -67,7 +67,7 @@ def product_detail(request, product_id):
 
 
 def add_product(request):
-    """Add a product to the store"""
+    """Add a product to the front end form"""
     if request.method == "POST":
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -82,6 +82,32 @@ def add_product(request):
     template = "products/add_product.html"
     context = {
         "form": form,
+    }
+
+    return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """Edit a product in the front end form"""
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Successfully updated realDream!")
+            return redirect(reverse("product_detail", args=[product.id]))
+        else:
+            messages.error(
+                request, "Failed to update. Please ensure the form is valid."
+            )
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f"You're now editing {product.name}")
+
+    template = "products/edit_product.html"
+    context = {
+        "form": form,
+        "product": product,
     }
 
     return render(request, template, context)
